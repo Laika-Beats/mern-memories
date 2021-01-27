3dfccbcb4cd8495d6ff9bc43c4a792574e81f8e3
Laika-Beats <laikabeatsdenver@gmail.com>
2021-01-27 09:00:42 -0700

    post layout for front-end

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Grid, CircularProgress } from "@material-ui/core";
 import { useSelector } from "react-redux";
 import Post from "./Post/Post";
 import useStyles from "./styles";
@@ -7,12 +8,21 @@ function Posts() {
   const posts = useSelector((state) => state.posts);
   const classes = useStyles();
   console.log(posts);
-  return (
-    <div>
-      <h1>POSTS</h1>
-      <Post />
-      <Post />
-    </div>
+  return !posts.length ? (
+    <CircularProgress />
+  ) : (
+    <Grid
+      className={classes.container}
+      container
+      alignItems="satretch"
+      spacing={3}
+    >
+      {posts.map((post) => (
+        <Grid key={post._id} item xs={12} sm={6}>
+          <Post post={post} />
+        </Grid>
+      ))}
+    </Grid>
   );
 }
 
