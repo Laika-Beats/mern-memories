35a00b75478642163754ea706e54dd22e79bc439
Laika-Beats <laikabeatsdenver@gmail.com>
2021-01-26 22:20:34 -0700

    connecting front-end to api

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Form from "./components/Form/Form";
 import Posts from "./components/Posts/Posts";
 import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import { useDispatch } from "react-redux";
+import { getPosts } from "./actions/posts";
 import useStyles from "./styles";
 
 function App() {
   const classes = useStyles();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPosts);
+  }, [dispatch]);
   return (
     <Container maxwidth="lg">
       <AppBar className={classes.appBar} position="static" color="inherit">
